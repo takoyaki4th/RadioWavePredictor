63a53db2e6c5e671e8d98ac81d71bf0f636c739e
takoyaki4th <kamagentadesu@gmail.com>
2025-08-13 01:42:06 +0900

    じかんをけいそくできるようにしました

--- hyperparam.py
+++ hyperparam.py
@@ -1,3 +1,7 @@
+import time
 from hyperparam_tuning.layer_unit_tuing import layer_unit_tuning
 
+start_time=time.time()
 layer_unit_tuning()
+end_time=time.time()
+print(f"実行時間:{(end_time-start_time):2f}秒")
